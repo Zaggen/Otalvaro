ac55c160b0347a7578d3886dca51ef64dd3d591c
Zaggen <xpynesoul@gmail.com>
2014-07-21 11:22:12 -0500

    added index data attribute

--- gallery.php
+++ gallery.php
@@ -2,69 +2,52 @@
 /*
  * Template Name: Gallery
  * */
+$page = $page ?: 1;
 $baseUri = getStylesheetUri();
-$gallery = array(
-    'images' => array(
-        $baseUri . 'imgs/gallery/0.jpg',
-        $baseUri . 'imgs/gallery/1.jpg',
-        $baseUri . 'imgs/gallery/2.jpg',
-        $baseUri . 'imgs/gallery/3.jpg',
-        $baseUri . 'imgs/gallery/4.jpg',
-        $baseUri . 'imgs/gallery/5.jpg',
-        $baseUri . 'imgs/gallery/6.jpg',
-        $baseUri . 'imgs/gallery/7.jpg',
-        $baseUri . 'imgs/gallery/8.jpg',
-        $baseUri . 'imgs/gallery/9.jpg',
-        $baseUri . 'imgs/gallery/10.jpg',
-        $baseUri . 'imgs/gallery/11.jpg',
-        $baseUri . 'imgs/gallery/0.jpg',
-        $baseUri . 'imgs/gallery/1.jpg',
-        $baseUri . 'imgs/gallery/2.jpg',
-        $baseUri . 'imgs/gallery/3.jpg',
-        $baseUri . 'imgs/gallery/4.jpg',
-        $baseUri . 'imgs/gallery/5.jpg',
-        $baseUri . 'imgs/gallery/6.jpg',
-        $baseUri . 'imgs/gallery/7.jpg',
-        $baseUri . 'imgs/gallery/8.jpg',
-        $baseUri . 'imgs/gallery/9.jpg',
-        $baseUri . 'imgs/gallery/10.jpg',
-        $baseUri . 'imgs/gallery/11.jpg',
-        $baseUri . 'imgs/gallery/0.jpg',
-        $baseUri . 'imgs/gallery/1.jpg',
-        $baseUri . 'imgs/gallery/2.jpg',
-        $baseUri . 'imgs/gallery/3.jpg',
-        $baseUri . 'imgs/gallery/4.jpg',
-        $baseUri . 'imgs/gallery/5.jpg',
-        $baseUri . 'imgs/gallery/6.jpg',
-        $baseUri . 'imgs/gallery/7.jpg',
-        $baseUri . 'imgs/gallery/8.jpg',
-        $baseUri . 'imgs/gallery/9.jpg',
-        $baseUri . 'imgs/gallery/10.jpg',
-        $baseUri . 'imgs/gallery/11.jpg'
-    )
+$galConf = array(
+    'imgPerPage' => 36,
+    'page' => $page
 );
 
+# We get our gallery array, which contains all the images data for the specified page (using the $galConf)
+$gallery = array();
+if(have_posts()):
+    while(have_posts()): the_post();
+        $gallery = getGallery($galConf);
+    endwhile;
+endif;
+
+
 if (isAjaxRequest()) {
-    jsonResponse($gallery);
+
+    $response = array(
+        'collection' => $gallery['galleryItems'],
+        'pageQ' => $gallery['meta']['pageQ']
+    );
+
+    jsonResponse($response);
 } else {
     get_header();
 ?>
+    <div id="galleryContent" class="grid">
+        <h2 class="navCrumb bioCrumb">Fotos  <i class="fa fa-camera"></i> </h2>
+        <ul class="gallery">
+            <?php
+            $i = 0;
+            foreach($gallery['galleryItems'] as $img){
+                echo "<li>
+                        <a href='{$img['title']}'>
+                           <img src='{$img['thumbnail']}' data-index='{$i}'/>
+                        </a>
+                     </li>";
+                $i++;
+            }
+            ?>
 
-    <h2 class="navCrumb bioCrumb">Fotos  <i class="fa fa-camera"></i> </h2>
-    <ul class="gallery">
-        <?php
-        foreach($gallery['images'] as $img){
-            echo "<li><img src='{$img}'/></li>";
-        }
-        ?>
+        </ul>
+    </div>
 
-    </ul>
-    <ul id="newsNavi" class="pageNavi">
-        <li class="navBtns selectedNav">1</li>
-        <li class="navBtns">2</li>
-        <li class="navBtns">3</li>
-        <li class="navBtns">4</li>
-    </ul>
+    <ul id="galNavi" class="pageNavi" data-page-quantity="<?= $gallery['meta']['pageQ']; ?>"></ul>
 
 <?php
     get_footer();
