defd1a8531d42423e33383f30726a6845e6fb0db
Zaggen <xpynesoul@gmail.com>
2014-07-24 15:47:53 -0500

    html template of the lightbox

---
+++ lightbox.php
@@ -0,0 +1,13 @@
+<div class="lightBox hidden" id="lightBox">
+    <div class="overlay"></div>
+
+    <div class="Window">
+        <div class="closeBtn"></div>
+        <div class="prevBtn"><i class="fa fa-angle-left"></i></div>
+        <div class="nextBtn"><i class="fa fa-angle-right fadeIn"></i></div>
+        <div class="progress">
+            <div>Loading…</div>
+        </div>
+        <figure id="lightboxContent"></figure>
+    </div>
+</div>
