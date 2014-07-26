ba0c9801e66969ea3d4d4efbc01a125723a7f462
Zaggen <xpynesoul@gmail.com>
2014-07-26 13:53:54 -0500

    Blog and gallery templates allow html content to be placed without scaping

--- underscore-templates.php
+++ underscore-templates.php
@@ -1,3 +1,4 @@
+<!-- HOME TEMPLATE -->
 <script id="homeTemplate" type="text/x-handlebars-template">
     <blockquote id="homeQuote"><p>I loved you yesterday, I love you still, I always have... I always will</p>
         <footer>Catalina Otalvaro</footer>
@@ -7,13 +8,8 @@
     </div>
 </script>
 
+<!-- BIO TEMPLATE -->
 <script id="bioTemplate" type="text/x-handlebars-template">
-    <div class="col_8">
-        <h2 class="navCrumb bioCrumb">{{ title }}
-        <i class="fa fa-user"></i>
-        </h2>
-        <p id="bioContent">{{ content }}</p>
-    </div>
     <div id="bioDetails" class="col_4"><h2 class="navCrumb">Perfil de Cata</h2>
         <figure id="bioPicture">
             <img src="{{ profileImg }}"/>
@@ -33,21 +29,19 @@
             <li><strong>Años de modelaje:</strong> 6</li>
         </ul>
     </div>
+    <div class="col_8 bioCol">
+        <h2 class="navCrumb bioCrumb">{{ title }}
+            <i class="fa fa-user"></i>
+        </h2>
+        <p id="bioContent">{{{ content }}}</p>
+    </div>
 </script>
 
-<script id="galleryTemplate" type="text/x-handlebars-template">
-    <h2 class="navCrumb bioCrumb">Fotos  <i class="fa fa-camera"></i> </h2>
-    <ul class="gallery">
-        {{#each galleryItems}}
-            <li><a href="{{this.fullImg}}" title="{{this.title}}"><img src="{{this.thumbnail}} "/></a></li>
-        {{/each}}
-    </ul>
- </script>
 
+<!-- BLOG TEMPLATES -->
 <script id="blogTemplate" type="text/x-handlebars-template">
-    <h2 class="navCrumb bioCrumb">Blog
-        <i class="fa fa-book"></i>
-    </h2>
+    <h2 class="navCrumb bioCrumb">Blog<i class="fa fa-book"></i></h2>
+    <ul id="blogFeed"></ul>
 </script>
 
 <script id="blogEntryTemplate" type="text/x-handlebars-template">
@@ -60,5 +54,49 @@
         <li><i class="fa fa-star"></i></li>
         <li><i class="fa fa-star"></i></li>
     </ul>
-    <div class="feedText">{{this.excerpt}}</div>
+    <div class="feedText">{{{this.excerpt}}}</div>
+</script>
+
+<!-- GALLERY TEMPLATE -->
+<script id="galleryTemplate" type="text/x-handlebars-template">
+    <h2 class="navCrumb bioCrumb">Fotos  <i class="fa fa-camera"></i> </h2>
+    <ul class="gallery transitionAll">
+        {{#each galleryItems}}
+        <li>
+            <a href="{{this.fullImg}}" title="{{this.title}}">
+                <img src="{{this.thumbnail}} " data-index="{{@index}}"/>
+            </a>
+        </li>
+        {{/each}}
+    </ul>
+</script>
+
+
+<!-- vIDEO GALLERY TEMPLATE -->
+<script id="videoGalleryTemplate" type="text/x-handlebars-template">
+    <h2 class="navCrumb bioCrumb">Videos  <i class="fa fa-film"></i> </h2>
+    <ul class="gallery transitionAll">
+        {{#each galleryItems}}
+        <li>
+            <a href="{{this.embedUrl}}" title="{{this.title}}">
+                <img src="{{this.thumbnail}} " data-index="{{@index}}"/>
+            </a>
+        </li>
+        {{/each}}
+    </ul>
+</script>
+
+<!-- LIGHTBOX TEMPLATE -->
+<script id="lightBoxTemplate" type="text/x-handlebars-template">
+    <div class="overlay"></div>
+    <div class="prevBtn"><i class="fa fa-angle-left"></i></div>
+    <div class="nextBtn"><i class="fa fa-angle-right fadeIn"></i></div>
+
+    <div class="window">
+        <div class="closeBtn"></div>
+        <div class="progress">
+            <div>Loading…</div>
+        </div>
+        <div id="lightboxContent"></div>
+    </div>
 </script>
